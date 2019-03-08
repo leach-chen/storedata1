57518cf1dac0137c4043b99364e7575085ded7dc
leach.chen <leach>
2019-03-08 17:07:05 +0800

    modify

---
+++ android/android.js
@@ -0,0 +1,32 @@
+{
+	"dataFull":[{
+			"thumbUrl": "https://www.leachchen.com/storeandroid1/1/android1_1_t.png",
+			"previewUrl": "https://www.leachchen.com/storeandroid1/1/android1_1_p.png",
+			"downloadUrl": "https://codeload.github.com/harjot-oberai/MusicDNA/zip/master",
+			"description": "一款舒适的音乐播放器",
+			"type": "android_full_1",
+			"author": "",
+			"authorurl": "https://github.com/harjot-oberai",
+			"gitauthor": "harjot-oberai",
+			"gitrepo": "MusicDNA",
+			"byauthor": "",
+			"byauthorurl": "",
+			"sold": true
+		}
+	],
+	"dataAnimation":[{
+			 "thumbUrl":"https://www.leachchen.com/storeandroid1/2/android1_2_t.jpg",
+			 "previewUrl":"https://www.leachchen.com/storeandroid1/2/android1_2_p.gif",
+			 "downloadUrl": "https://codeload.github.com/Airbnb/lottie-android/zip/master",
+			 "description":"airbnb的lottie酷炫动画库",
+			 "type":"android_animation_1",
+			 "author":"",
+			 "authorurl":"https://github.com/airbnb",
+			 "gitauthor":"Airbnb",
+			 "gitrepo":"lottie-android",
+			 "byauthor":"",
+			 "byauthorurl":"",
+			 "sold":true
+		  }
+	]
+}
