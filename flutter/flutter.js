af4fb18cc5c957e1a427c7973be8d44ad492accb
LAPTOP-8CFJ92M4\34976 <349763031@qq.com>
2019-03-12 23:07:45 +0800

    add

---
+++ flutter/flutter.js
@@ -0,0 +1,17 @@
+{
+	"dataFlutterFull1":[{
+			"thumbUrl":"https://www.leachchen.com/storeflutter1/1/flutter1_1_t.jpg",
+			"previewUrl":"https://www.leachchen.com/storeflutter1/1/flutter1_1_p.gif",
+			"downloadUrl": "https://codeload.github.com/alibaba/flutter-go/zip/master",
+			"description":"阿里Flutter学习项目，组件丰富",
+			"type":"flutter_full_1",
+			"author":"",
+			"authorurl":"https://github.com/alibaba/",
+			"gitauthor":"alibaba",
+			"gitrepo":"flutter-go",
+			"byauthor":"",
+			"byauthorurl":"",
+			"sold":true
+		}
+	]
+}
