0862ccfde923ac424fdaabc1cea9250e365298ac
LAPTOP-8CFJ92M4\34976 <349763031@qq.com>
2019-03-30 22:53:21 +0800

    modify

--- flutter/flutter.js
+++ flutter/flutter.js
@@ -1,5 +1,21 @@
 {
 	"dataFlutterFull1":[{
+    "id":"flutter_2",
+    "thumbUrl":"https://www.leachchen.com/storeflutter1/2/flutter1_2_t.jpg",
+    "previewUrl":"https://www.leachchen.com/storeflutter1/2/flutter1_2_p.jpg",
+    "downloadUrl":"https://codeload.github.com/MeandNi/Flutter_ZhiHu/zip/master",
+    "description":"开发知乎App的Flutter版。技术栈：flutter+redux+sqflite。",
+    "type":"flutter_full_1",
+    "author":"",
+    "authorurl":"https://github.com/MeandNi",
+    "gitauthor":"MeandNi",
+    "gitrepo":"Flutter_ZhiHu",
+    "byauthor":"",
+    "byauthorurl":"",
+    "sold":true,
+    "tag":"flutter",
+  "time":"20190330225248"
+  },{
 		  "id":"flutter_1",
 		  "time":"",
 			"thumbUrl":"https://www.leachchen.com/storeflutter1/1/flutter1_1_t.jpg",
